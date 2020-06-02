c4e2ce1e91b91f3423e1a7d3fc2530a32e192569
Steven Barnes <ssbarnes21@gmail.com>
2020-06-02 17:54:13 +0100

    Added getters and setters for angle and length. Also added index.html and app.js for testing purposes. Added export default to vector.js

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict"
+
+import Vector from './vector.js';
+
+window.onload = () => {
+  let v1 = new Vector();
+  console.log(v1.getX(), v1.getY());
+};
--- vector.js
+++ vector.js
@@ -1,6 +1,6 @@
 "use strict"
 
-class Vector {
+export default class Vector {
   constructor() {
     this._x = 1;
     this._y = 0;
@@ -20,6 +20,26 @@ class Vector {
 
   getY() {
     return this._y;
+  }
+
+  getAngle() {
+    return Math.atan2(this._y, this._x);
+  }
+
+  setAngle(angle) {
+    let length = this.getLength();
+    this._x = Math.cos(angle) * length;
+    this._y = Math.sin(angle) * length;
+  }
+
+  getLength() {
+    return Math.hypot(this._x, this._y);
+  }
+
+  setLength(length) {
+    let angle = this.getAngle();
+    this._x = Math.cos(angle) * length;
+    this._y = Math.sin(angle) * length;
   }
 }
 
