463aba875fba308cc5b40828e30ac7b7e499afc7
Steven Barnes <ssbarnes21@gmail.com>
2020-06-02 17:26:09 +0100

    Added getters and setters for _x and _y

--- vector.js
+++ vector.js
@@ -5,4 +5,40 @@ class Vector {
     this._x = 1;
     this._y = 0;
   }
+
+  setX(x) {
+    this._x = x;
+  }
+
+  setY(y) {
+    this._y = y;
+  }
+
+  getX() {
+    return this._x;
+  }
+
+  getY() {
+    return this._y;
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
